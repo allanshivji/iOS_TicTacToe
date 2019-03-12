a233b166dfcd89fbf7284845374fb78f4a28ccc1
allanshivji <allanmshj@gmail.com>
2019-03-12 00:43:17 -0400

    second commit

--- My Tic Tac Toe/AppDelegate.swift
+++ My Tic Tac Toe/AppDelegate.swift
@@ -12,7 +12,7 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-    var mpcHandler:MPCHandler = MPCHandler()
+    var mpcHandling = MultipeerConnectivityHandling()
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
--- My Tic Tac Toe/ViewController.swift
+++ My Tic Tac Toe/ViewController.swift
@@ -7,14 +7,60 @@
 //
 
 import UIKit
+import MultipeerConnectivity
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MCBrowserViewControllerDelegate {
+    
+    
 
+    @IBOutlet var xField: [MyImageView]!
+    
+    var appDelegate:AppDelegate!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        appDelegate = UIApplication.shared.delegate as? AppDelegate
+        appDelegate.mpcHandling.gettingPeerConnection(displayName: UIDevice.current.name)
+        appDelegate.mpcHandling.gettingSession()
+        appDelegate.mpcHandling.showingSelf(show: true)
+    }
+    
+    
+    @IBAction func getConnected(_ sender: Any) {
+        
+        if appDelegate.mpcHandling.gameSession != nil {
+            appDelegate.mpcHandling.gettingBrowser()
+            appDelegate.mpcHandling.browser.delegate = self
+            
+            self.present(appDelegate.mpcHandling.browser, animated: true, completion: nil
+            )
+            
+        }
+        
+    }
+    
+    
+    
+    
+    func mainLogic() {
+        for index in 0 ... xField.count - 1 {
+            
+            let recognizeGestureTouch = UITapGestureRecognizer(target: self, action: "fieldTapped")
+            
+            recognizeGestureTouch.numberOfTapsRequired = 1
+            
+            xField[index].addGestureRecognizer(recognizeGestureTouch)
+        }
     }
 
+    func browserViewControllerDidFinish(_ browserViewController: MCBrowserViewController) {
+        appDelegate.mpcHandling.browser.dismiss(animated: true, completion: nil)
+    }
+    
+    func browserViewControllerWasCancelled(_ browserViewController: MCBrowserViewController) {
+        appDelegate.mpcHandling.browser.dismiss(animated: true, completion: nil)
+    }
 
 }
 
